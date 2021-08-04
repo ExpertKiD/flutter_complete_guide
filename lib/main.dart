5508aea45232e8142282568af1689e065d0ab0ed
Suman Adhikari <expertxkid@gmail.com>
2021-08-04 21:11:09 +0545

    Add reset button on Result widget

--- lib/main.dart
+++ lib/main.dart
@@ -46,6 +46,13 @@ class _MyAppState extends State<MyApp> {
     },
   ];
 
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
   void _answerQuestion(int score) {
     _totalScore += score;
 
@@ -75,7 +82,7 @@ class _MyAppState extends State<MyApp> {
               questions: _questions,
               questionIndex: _questionIndex,
             )
-          : Result(_totalScore),
+          : Result(_totalScore, _resetQuiz),
     ));
   }
 }
